9b62a7556995f0e4a4dcff0b940d301ce8b5e66f
Mads Sejersen <mas@issuu.com>
2016-06-02 11:46:52 +0200

    Respect letter spacing

--- render/index.js
+++ render/index.js
@@ -1,77 +1,77 @@
 var JSON_FILE = 'bs15_3/text.json';
 var JPEG_FILE = 'bs15_3/bg3.png';
+// var JPEG_FILE = 'http://image.issuu.com/150802204627-f6b514e5a5aeb6ab3c493f07a6eee7bd/jpg/page_3.jpg';
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d', { alpha: false });
+ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-var fonts = {};
 
 function draw(img, textboxes) {
     // Fit canvas to image
     canvas.width = img.width;
     canvas.height = img.height;
     ctx.drawImage(img, 0, 0);
-    // ctx.fillStyle = 'white';
-    // ctx.fillRect(0, 0, img.width, img.height);
 
-    // Scale ctx so pixels match pdf points (assume A4)
-    // var pdfWidth = 595, pdfHeight = 842; // in postscript points
-    var pdfWidth = 481.89, pdfHeight = 680.31;
+    // Scale ctx so pixels match pdf points
+    // var pdfWidth = 595, pdfHeight = 842; // A4 in postscript points
+    var pdfWidth = 481.89, pdfHeight = 680.31; // Boneshaker dimensions in pts
     ctx.scale(img.width/pdfWidth, img.height/pdfHeight);
 
-    var previousWidth = 0;
+    // Where to draw the next character
+    var x = 0;
 
     textboxes.forEach(function(textbox) {
-        if (fonts[textbox.font_id] === undefined) {
-            var css = "@font-face {" + [
-                'font-family: "f' + textbox.font_id + '";',
-                'src: url("bs15_3/f' + textbox.font_id + '.woff")',
-                'format("woff");'
-            ].join(' ') + "}";
-            var style = document.createElement('style');
-            style.type = "text/css";
-            style.appendChild(document.createTextNode(css));
-            document.getElementsByTagName('head')[0].appendChild(style);
-            fonts[textbox.font_id] = null;
-        }
-
         ctx.save();
 
         if (!textbox.append) {
-            previousWidth = 0;
+            x = 0;
         }
 
+        ctx.font = textbox.font_size + 'px f' + textbox.font_id;
+        ctx.fillStyle = textbox.fill_color;
+        // ctx.fillStyle = 'blue';
+        ctx.strokeStyle = textbox.stroke_color;
+
+        // Apply transformation matrix and position
         ctx.transform(
             textbox.transform[0],
             textbox.transform[1],
             textbox.transform[2],
             textbox.transform[3],
-            textbox.x, pdfHeight - textbox.y
+            textbox.x,
+            pdfHeight - textbox.y // FIXME: Do this in the backend
         );
-        ctx.transform(1, 0, 0, 1, previousWidth, 0);
-        ctx.font = textbox.font_size + 'px f' + textbox.font_id;
-        ctx.fillStyle = textbox.fill_color;
-        ctx.fillText(textbox.text, 0, 0);
-        ctx.strokeStyle = textbox.stroke_color;
-        ctx.strokeText(textbox.text, 0, 0);
 
-        metrics = ctx.measureText(textbox.text);
-        previousWidth += metrics.width;
+        var remainingWidth = ctx.measureText(textbox.text).width;
+
+        for (var i = 0; i < textbox.text.length; i++) {
+            var letter = textbox.text[i];
+            ctx.fillText(letter, x, 0);
+            ctx.strokeText(letter, x, 0);
+            var w = ctx.measureText(textbox.text.substr(i+1)).width;
+            x += remainingWidth - w + textbox.letter_space; // + (/\s/.test(letter) ? textbox.word_space : 0);
+            remainingWidth = w;
+        }
 
         ctx.restore();
     });
 }
 
-function loadFont(url) {
-    return new Promise(function(resolve, reject) {
-        var link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-        link.href = url;
-        document.getElementsByTagName('head')[0].appendChild(link);
-        resolve(); // FIXME: Figure out when font is actually ready
-    });
+var fonts = {};
+function loadFont(id) {
+    if (fonts[id] === undefined) {
+        var css = "@font-face {" + [
+            'font-family: "f' + id + '";',
+            'src: url("bs15_3/f' + id + '.woff")',
+            'format("woff");'
+        ].join(' ') + "}";
+        var style = document.createElement('style');
+        style.type = "text/css";
+        style.appendChild(document.createTextNode(css));
+        document.getElementsByTagName('head')[0].appendChild(style);
+        fonts[id] = null;
+    }
 }
 
 
@@ -94,6 +94,9 @@ var textboxesPromise = fetch(JSON_FILE).then(function(rsp) {
 Promise.all([imagePromise, textboxesPromise]).then(function(values) {
     var img = values[0];
     var textboxes = values[1];
+    textboxes.forEach(function(textbox) {
+        loadFont(textbox.font_id);
+    });
     draw(img, textboxes);
     setInterval(function() {
         draw(img, textboxes);
